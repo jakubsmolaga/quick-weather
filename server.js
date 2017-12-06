1e72233a842adc19f747243d38d76be04254a79e
smolagakuba <smolagakuba@gmail.com>
2017-12-06 22:18:13 +0100

    cleaning up

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const hbs = require('hbs');
-const fs = require('fs');
 
 const geocode = require('./geocode/geocode');
 const weather = require('./weather/weather');
@@ -11,48 +10,14 @@ let app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
-app.use((req, res, next)=>{
-  var now = new Date().toString();
-  var log = `${now}: ${req.method} ${req.url}`;
-
-  console.log(log);
-  fs.appendFile('server.log', log + '\n', (e)=>{
-    if(e)console.log('Unable to append to server.log.');
-  })
-  next();
-});
-//app.use((req, res, next)=>{
-//  res.render('maintenance.hbs');
-//});
-app.use(express.static(__dirname + '/public'));
-
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
 
-hbs.registerHelper('screamIt', (text)=>{
-  return text.toUpperCase();
-});
-
 app.get('/', (req, res)=>{
-  res.render('home.hbs', {
-    pageTitle: 'Home page',
-    welcomeMessage: 'What address do you want to check the info for?'
-  });
+  res.render('home.hbs');
 });
 
-app.get('/about', (req, res) => {
-  res.render('about.hbs', {
-    pageTitle: 'About Page',
-  });
-});
-
-app.get('/bad', (req, res) => {
-  res.send({
-    errorMessage: 'Something went wrong'
-  });
-});
-//
 app.get('/search', (req, res) => {
   a = req.query['a'];
   geocode.geocodeAddress(a, (err, results)=>{
@@ -60,20 +25,21 @@ app.get('/search', (req, res) => {
       console.log(err);
       res.send('Somethin went wrong. Try refreshing the page.');
     }else{
-      console.log(results.address);
       weather.getWeather(results.latitude, results.longitude, (err, weatherResults)=>{
-        console.log(weatherResults.summary);
-        res.render('result.hbs', {
-         address: results.address,
-         latitude: results.latitude,
-         longitude: results.longitude,
-         temperature: weatherResults.temperature,
-         apparentTemperature: weatherResults.apparentTemperature,
-         windSpeed: weatherResults.windSpeed,
-         summary: weatherResults.summary
-        });
+        if(err){
+          res.send('Something went wrong. Try refreshing the page.');
+        } else {
+          res.render('result.hbs', {
+            address: results.address,
+            latitude: results.latitude,
+            longitude: results.longitude,
+            temperature: weatherResults.temperature,
+            apparentTemperature: weatherResults.apparentTemperature,
+            windSpeed: weatherResults.windSpeed,
+            summary: weatherResults.summary
+          });
+        }
       });
-
     }
   });
 });
